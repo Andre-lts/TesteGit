14e849e3462e674da0d04e2373c9912942118d09
FIC <fic@info.local>
2025-02-05 21:22:21 -0300

    Arquivo inicial adicionado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+//Exercicio 1
+/*Console.WriteLine("Insira o comprimento");
+double comprimento = double.Parse(Console.ReadLine());
+Console.WriteLine("Insira a largura");
+double largura = double.Parse(Console.ReadLine());
+
+double area = comprimento * largura;
+
+Console.WriteLine("A area do terreno e: " + area +"m2");*/
+
+// Exercico 2
+/*Console.WriteLine("Digite a sua idade");
+int idade = int.Parse(Console.ReadLine());
+
+int diasVida = idade * 365;
+
+Console.WriteLine("Voce tem " + diasVida + " dias de vida");*/
+
+// Exercico 3
+/*Console.WriteLine("Digite a sua idade");
+int idade = int.Parse(Console.ReadLine());
+
+int diasVida = idade * 365;
+int mesesVida = idade * 12;
+
+Console.WriteLine("Voce tem " + diasVida + " dias e "+ mesesVida+" meses de vida");*/
+
+//Exercicio 4
+Console.WriteLine("Insira a primeira nota");
+int nota1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Insira a segunda nota");
+int nota2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Insira a terceira nota");
+int nota3 = int.Parse(Console.ReadLine());
+
+double media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine("A media do aluno e: " + media);
+
